7105df703fa27bddd1314e23ea7daf03c261c7f6
Lakshmi Parthasarathy <lakshmi122290@gmail.com>
2014-05-14 11:48:22 -0400

    First Commit!
    Sessions working, tested in fake app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+end
